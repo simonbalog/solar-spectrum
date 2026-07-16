a1d4d57a47af3f866b3856709dd4493f8c13b30c
simonbalog <simonbalog11@seznam.cz>
2026-07-16 20:44:55 +0200

    Fix merge conflicts - keep Mellotech version

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://simonbalog.github.io',
-<<<<<<< HEAD
   base: '/mellotech',
-=======
-  base: '/solar-spectrum',
->>>>>>> 4c4af82f5d52777863c8bbe3a0dfe4695fdb1612
 });
